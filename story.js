cf8c8870e675f162a2013cb822501bc14f67f2b9
ReyPena <zonickblack@gmail.com>
2015-06-11 21:03:43 -0600

    file structure and js files

---
+++ story.js
@@ -0,0 +1,45 @@
+// THis is my story
+//the verbs, adjetives, and nouns.
+
+var verb_Present_ends_inING1 = prompt("Please write a verb in Present that ends in ing.");
+var verbPastTense1 = prompt("Please write a verb in past tense.");
+var Name_of_crush = prompt("Please write The name of the person you are in love.");
+var adjetive1 = prompt("Please write an adjetive");
+var color = prompt("please write a color.");
+var verb_Present_ends_inING2 = prompt("Please write a verb in Present that ends in ing.");
+var verbPastTense2 = prompt("Please write a verb in past tense.");
+var adjetive2 = prompt("Please write an adjetive");
+var verbPastTense3 = prompt("Please write a verb in past tense.");
+var verbBasedForm = prompt("Please write a verb on its base from.");
+var verbPastTense4 = prompt("Please write a verb in past tense.");
+var body_part1 = prompt("Please write the name of one part of the body.");
+var body_part2 = prompt("Please write the name of one part of the body.");
+var body_part3 = prompt("Please write the name of one part of the body.");
+var exercise = prompt("Please write your favorite exercise.");
+var noun = prompt("please write a noun");
+var ClothingPiece1 = prompt("Please write a chlothing piece.");
+var ClothingPiece2 = prompt("Please write a chlothing piece.");
+var ClothingPiece3 = prompt("Please write a chlothing piece.");
+var body_part4 = prompt("Please write the name of one part od the body.");
+var ClothingPiece4 = prompt("Please write a chlothing piece.");
+var anyNumber = prompt("Please write a any number");
+
+// The story
+
+//First paragraph
+document.write("One day, I was " + verb_Present_ends_inING1 + " on the sidewalk when I " + verbPastTense1 + " into " + Name_of_crush + "." + " Gosh, how " + adjetive1 + " those " + color + " eyes are. I started " + verb_Present_ends_inING2 + " because that was my crush.");
+
+//second paragraph
+document.write("That night, I " + verbPastTense2 + " about " + Name_of_crush + ". " + "How it would be if we were " + adjetive2 + ". How it would be if we had sex..");
+
+//third paragraph
+document.write("The next day, I " + verbPastTense3 + " the guts to " + verbBasedForm + " " + Name_of_crush + " over to my house. We " + verbPastTense4 + ", and she seemed interested in me. I realized that I had been staring at her pretty " + body_part1 + " the whole time. I asked her if she wanted to come to my room. She looked at me quizzically, but said she would anyways. When we got in my room, she put her " + body_part2 + " on my " + body_part3 + " and said that she loved me since we first met. My heart started to do " + exercise + ", and I told " + Name_of_crush + " the same. Suddenly, my " + noun + " went up and I asked her if she wanted to see more of me. She said yes. She slowly unzipped my " + ClothingPiece1 + " as I unbuttoned her " + ClothingPiece2 + " and undid her " + ClothingPiece3 + ". Those " + body_part4 + " were so perfect. She took off my " + ClothingPiece4 + " and we got on the bed when we were naked. We had the best sex ever. It lasted " + anyNumber + " minutes. I was so happy.");
+
+//The last line
+document.write(" Now we go out every week, and have sex every time we get home.");
+
+
+
+//The alert for the end               
+               
+alert("You're done!");
